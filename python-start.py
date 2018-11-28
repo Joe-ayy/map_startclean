1db913a7e59d10ac332839b050c944306a2a7c6f
jarjaris <38326893+jarjaris@users.noreply.github.com>
2018-11-28 16:48:53 -0500

    Add files via upload
    Fixed the brush and many bugs

--- python-start.py
+++ python-start.py
@@ -3,6 +3,10 @@
 
 from gimpfu import *
 import os
+import sys
+
+sys.stderr = open('C:\\Users\\100062393\\Documents\\start_testing.txt', 'w')
+sys.stdout = open('C:\\Users\\100062393\\Documents\\start1_debris_testing.txt', 'w')
 
 #delets yaml if one exsists to avoid confusion
 #def delete_old_yaml(directory):
@@ -10,7 +14,6 @@ import os
 #	for file in files:
 #		if file.endswith(".yaml"):
 #			os.remove(file)
-
 
 #paints a circle around the park or dock location by makeing a bush and painting with it
 def paint_hazard(drw,x,y):
@@ -23,14 +26,14 @@ def paint_hazard(drw,x,y):
 	pdb.gimp_context_set_foreground(hazard_color)
 	temp_brush = pdb.gimp_brush_new(temp_brush)
 	pdb.gimp_brush_set_shape(temp_brush, 0)
-	pdb.gimp_brush_set_radius(temp_brush, 1000)
-	pdb.gimp_context_set_brush_size(hazard_radius)
 	pdb.gimp_brush_set_hardness(temp_brush, 100)
 	pdb.gimp_context_set_opacity(50)
+	pdb.gimp_context_set_brush(temp_brush)
+	pdb.gimp_context_set_brush_size(hazard_radius)
 	pdb.gimp_pencil(drw, 2, center)
 	pdb.gimp_context_set_background(og_background)
 	pdb.gimp_context_set_foreground(og_foreground)
-	pdb.gimp_brush_delete(temp_brush)
+	#pdb.gimp_brush_delete(temp_brush)
 	pdb.gimp_context_set_opacity(100)
 
 	
@@ -48,19 +51,14 @@ def check_clearence(drw, img, start_x, start_y, side_length):
 		return True
 		
 
-
-def convert_meters_to_pixels(meters, ratio):
-	return meters / ratio
-
-
 def parse_x_y(line, x_y_list):
-	# takes in a line and returns the x,y coordinates found in it
+	# takes in a line and returns the float x,y coordinates found in it
 	pos1 = line.find(' ')
 	x_meters_string = line[:pos1]
 	pos2 = line.find(' ', pos1 + 1)
 	y_meters_string = line[pos1 + 1:pos2]
-	x_y_list.append(x_meters_string)
-	x_y_list.append(y_meters_string)
+	x_y_list.append(float(x_meters_string))
+	x_y_list.append(float(y_meters_string))
 	return
 
 
@@ -69,6 +67,8 @@ def get_start_stop_pixels(filename, ratio, offset_list):
 	start_and_stop_list = []
 	park_x_y = []
 	dock_x_y = []
+	park_x_y_m = []
+	dock_x_y_m = []
 	file = open(filename, 'r')
 	line = file.readline()
 	#hard code skip the header in the file
@@ -83,66 +83,40 @@ def get_start_stop_pixels(filename, ratio, offset_list):
 		
 	last_line = line_list[len(line_list) - 1]
 	parse_x_y(last_line, dock_x_y)
-	#convert strings to float
-	park_x_meters_string = park_x_y[0]
-	park_x_meters_float = float(park_x_meters_string)
-	park_y_meters_string = park_x_y[1]
-	park_y_meters_float = float(park_y_meters_string)
-	dock_x_meters_string = dock_x_y[0]
-	dock_x_meters_float = float(dock_x_meters_string)
-	dock_y_meters_string = dock_x_y[1]
-	dock_y_meters_float = float(dock_y_meters_string)
 	
-	# convert from meters to pixels
-	park_x_pixels_float = convert_meters_to_pixels(park_x_meters_float, ratio)
-	park_y_pixels_float = convert_meters_to_pixels(park_y_meters_float, ratio)
-	dock_x_pixels_float = convert_meters_to_pixels(dock_x_meters_float, ratio)
-	dock_y_pixels_float = convert_meters_to_pixels(dock_y_meters_float, ratio)
+	park_x_y_m.append(park_x_y[0])
+	park_x_y_m.append(park_x_y[1])
+	dock_x_y_m.append(dock_x_y[0])
+	dock_x_y_m.append(dock_x_y[1])
 	
-	#round to nearest pixel because they are ints
-	park_x_pixels_int = int(round(park_x_pixels_float))
-	park_y_pixels_int = int(round(park_y_pixels_float))
-	dock_x_pixels_int = int(round(dock_x_pixels_float))
-	dock_y_pixels_int = int(round(dock_y_pixels_float))
+	park_x_y[0] = int(round(park_x_y[0] * ratio)) + offset_list[0]
+	park_x_y[1] = int(round(park_x_y[1] * ratio)) + offset_list[1]
 	
-	#get actual location in pixels by added offset
-	park_x_pixels_int = park_x_pixels_int + offset_list[0]
-	park_y_pixels_int = park_y_pixels_int + offset_list[1]
-	dock_x_pixels_int = dock_x_pixels_int + offset_list[0]
-	dock_y_pixels_int = dock_y_pixels_int + offset_list[1]
+	dock_x_y[0] = int(round(dock_x_y[0] * ratio)) + offset_list[0]
+	dock_x_y[1] = int(round(dock_x_y[1] * ratio )) + offset_list[1]
 	
-	# get actual location in meters for the file name
-	park_x_meters_float = park_x_meters_float + offset_list[0] * ratio
-	park_y_meters_float = park_y_meters_float + offset_list[1] * ratio
-	dock_x_meters_float = dock_x_meters_float + offset_list[0] * ratio
-	dock_y_meters_float = dock_y_meters_float + offset_list[1] * ratio
+	# get actual location in meters for the file name, either offset or ratio need to be casted to a float or else they return an int
+	
+	park_x_y_m[0] = park_x_y_m[0] + float(offset_list[0]) / ratio
+	park_x_y_m[1] = park_x_y_m[1] + float(offset_list[1]) / ratio
+	
+	
+	dock_x_y_m[0] = dock_x_y_m[0] + float(offset_list[0]) / ratio
+	dock_x_y_m[1] = dock_x_y_m[1] + float(offset_list[1]) / ratio
 	
 	# build the list 
-	start_and_stop_list.append(park_x_pixels_int)
-	start_and_stop_list.append(park_y_pixels_int)
-	start_and_stop_list.append(dock_x_pixels_int)
-	start_and_stop_list.append(dock_y_pixels_int)
-	start_and_stop_list.append(park_x_meters_float)
-	start_and_stop_list.append(park_y_meters_float)
-	start_and_stop_list.append(dock_x_meters_float)
-	start_and_stop_list.append(dock_y_meters_float)
+	start_and_stop_list.append(park_x_y[0])
+	start_and_stop_list.append(park_x_y[1])
+	start_and_stop_list.append(dock_x_y[0])
+	start_and_stop_list.append(dock_x_y[1])
+	
+	
+	start_and_stop_list.append(park_x_y_m[0])
+	start_and_stop_list.append(park_x_y_m[1])
+	start_and_stop_list.append(dock_x_y_m[0])
+	start_and_stop_list.append(dock_x_y_m[1])
 	
 	return start_and_stop_list
-
-
-def get_pixel_ratio(file_name):
-# take in file name and return the meters/pixels ratio as a float
-	file = open(file_name, 'r')
-	line = file.readline()
-	for x in range(0,3):
-		line = file.readline()
-		
-	delimiter_location1 = line.find(' ')
-	delimiter_location2 = line.find('\n')
-	ratio_string = line[delimiter_location1 + 1: delimiter_location2]
-	ratio_float = float(ratio_string)
-	file.close()
-	return ratio_float
 
 def get_pixel_offset(file_name):
 # takes file name and returns the offset points in a list
@@ -165,7 +139,7 @@ def get_pixel_offset(file_name):
 	file.close()
 	return offset_list
 
-def parse_files(dir_path):
+def parse_files(dir_path,ratio):
 #takes the path to the directory and returns the park and dock list
 	# look for .info and .ply file in the directory
 	info_file_name = ""
@@ -175,14 +149,14 @@ def parse_files(dir_path):
 			ply_filename = file
 		if file.endswith(".info"):
 			info_file_name = file
-
+					
 	info_file_name = dir_path + "/" + info_file_name
 	ply_filename = dir_path + "/" + ply_filename
-	ratio = get_pixel_ratio(info_file_name)
 	x_y_offset = get_pixel_offset(info_file_name)
 	start_stop_pixel_list = get_start_stop_pixels(ply_filename, ratio, x_y_offset)
 	return start_stop_pixel_list
-   
+
+
 def draw_square(drw,x,y,color):
 #take in center x,y and draw a 9 x  9 square around it
 	for z in range(0,4):
@@ -191,10 +165,12 @@ def draw_square(drw,x,y,color):
 			pdb.gimp_drawable_set_pixel(drw,x - z,y - w,3,color)
 			pdb.gimp_drawable_set_pixel(drw,x + z,y - w,3,color)
 			pdb.gimp_drawable_set_pixel(drw,x - z,y + w,3,color)
-	
+
 
 def python_start(img,drw):
 	# init colors and image
+	# The pixel to meter ratio is 1 pixel = 0.05 meters or 20 pixels = 1 meter
+	m2p = 20
 	park_color = [204,0,0]
 	dock_color = [0,0,204]
 	img = gimp.image_list()[0]
@@ -218,7 +194,7 @@ def python_start(img,drw):
 	
 	
 	#call to create the xy list of points and then draw those
-	x_y_list = parse_files(dir_path)
+	x_y_list = parse_files(dir_path,m2p)
 	park_spot_clean_flag = check_clearence(drw,img,x_y_list[0],x_y_list[1],clearance_diameter)
 	dock_spot_clear_flag = check_clearence(drw,img,x_y_list[2],x_y_list[3],clearance_diameter)
 	
